7506619d8d97e05b8537086eb5c313bb475abe3b
Jason Worden <wordenjasonp@gmail.com>
2016-11-10 12:57:28 -0800

    Set up font-awesome

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
+import 'font-awesome/css/font-awesome.css';
 import './app.css';
 import styles from './styles.module.css';
 
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className={styles.wrapper}>
-        wot is life doe
+        wot is life doe <i className="fa fa-star"/>
         <h1>env: {__NODE_ENV__}</h1>
       </div>
     );
