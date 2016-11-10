0561d362c3a84ecbce67b1c3940bc71226977f41
Jason Worden <wordenjasonp@gmail.com>
2016-11-10 11:09:26 -0800

    Setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const getConfig = require('hjs-webpack');
+const fs = require('fs');
+const path = require('path'),
+  join = path.join,
+  resolve = path.resolve;
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest = join(root, 'dist');
+
+var config = getConfig({
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+});
