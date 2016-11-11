09b6391623d1db2bbddd397b15e1dd2203386bba
Jason Worden <wordenjasonp@gmail.com>
2016-11-11 10:16:12 -0800

    Added simple google map component

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -3,10 +3,11 @@ import Map, { GoogleApiWrapper } from 'google-maps-react';
 
 export class Container extends Component {
   render() {
-    console.log('gapi:', __GAPI_KEY__)
+    //wrapping Container in GoogleApiWrapper gives it a prop of google
     return (
       <div>
         Hello I am tupperware container hi hello
+        <Map google={this.props.google} />
       </div>
     )
   }
